16f58fefa7992062b3a86c923543ba543e15d479
Hamed <hamed.mendscole@mymail.champlain.edu>
2025-09-26 16:14:06 -0400

    C:/Program Files/Git/week2/

---
+++ Week 4/ApachesParsing.ps1
@@ -0,0 +1,23 @@
+﻿function ApacheLogs1() {
+$logsNotformatted = Get-Content c:\xampp\apache\logs\access.log
+$tableRecords = @()
+
+for($i=0; $i -lt $logsNotformatted.Count; $i++){
+
+# split a string into words
+$words = $logsNotformatted[$i].split(" ");
+
+$tableRecords += [pscustomobject]@{ "IP" = $words[0];
+                                   "Time" = $words[3].Trim('[');
+                                   "Method" = $words[5].Trim('"');
+                                   "Page" = $words[6];
+                                   "Protocol" = $words[7];
+                                   "Response" = $words[8];
+                                   "Referrer" = $words[10];
+                                   "Client" = $words[11..($words.length-1)]; } 
+
+} # end of for loop
+return $tableRecords | Where-Object { $_.IP -ilike "10.*" }
+}
+$tableRecords = ApacheLogs1
+$tableRecords | Format-Table -Autosize -Wrap
---
+++ Week 4/HTML ELEMENTS.ps1
@@ -0,0 +1,21 @@
+﻿
+<#$scraped_page = Invoke-WebRequest -TimeoutSec 10 http://localhost/ToBeScrapted.html
+
+# Get a count of links in the page 
+$scraped_page.Links.count
+
+#Display URL only
+
+$scraped_page.Links
+
+$scraped_page.Links | Select-Object outertext, href
+#>
+#$h2s = $scraped_page.ParsedHtml.body.getElementsBytagname("h2") | Select-Object outertext
+#$h2s
+
+$divs1=$scraped_page.ParsedHtml.body.getElementsByTagName("div") | where {
+$_.getAttributeNode("class").value -ilike "div-1" 
+} | Select-Object innertext
+
+$divs1
+
